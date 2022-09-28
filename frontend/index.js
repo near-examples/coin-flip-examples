54cc0c18b1b6411c601526397cf627536292901d
gagdiez <gagdiez.c@gmail.com>
2022-09-28 18:32:18 +0200

    simplified frontend

--- frontend/index.js
+++ frontend/index.js
@@ -3,7 +3,8 @@ import { Wallet } from './near-wallet';
 
 // When creating the wallet you can optionally ask to create an access key
 // Having the key enables to call non-payable methods without interrupting the user to sign
-const wallet = new Wallet({ createAccessKeyFor: process.env.CONTRACT_NAME })
+const CONTRACT_ADDRESS = process.env.CONTRACT_NAME
+const wallet = new Wallet({ createAccessKeyFor: CONTRACT_ADDRESS })
 
 // Setup on page load
 window.onload = async () => {
