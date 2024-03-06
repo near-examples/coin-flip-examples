e95981413d140e207575f724304ee9bbc0f515d8
garikbesson <garikbesson@gmail.com>
2024-03-06 21:31:40 +0100

    update frontend

--- frontend/index.js
+++ frontend/index.js
@@ -3,7 +3,7 @@ import {Wallet} from './near-wallet';
 
 // When creating the wallet you can optionally ask to create an access key
 // Having the key enables to call non-payable methods without interrupting the user to sign
-const CONTRACT_ADDRESS = process.env.CONTRACT_NAME
+const CONTRACT_ADDRESS = process.env.CONTRACT_NAME || "coin-flip.near-examples.near"
 const wallet = new Wallet({ createAccessKeyFor: CONTRACT_ADDRESS })
 
 // Setup on page load
