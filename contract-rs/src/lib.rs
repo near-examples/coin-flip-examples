b5c429e28ac558ff84f4c82dba803c86df92dc3b
Lyudmil Ivanov <55487633+flmel@users.noreply.github.com>
2024-03-29 09:35:48 +0200

    chore: update rust_sdk

--- contract-rs/src/lib.rs
+++ contract-rs/src/lib.rs
@@ -1,31 +1,28 @@
 // Find all our documentation at https://docs.near.org
-use near_sdk::borsh::{BorshSerialize, BorshDeserialize};
-use near_sdk::env::{self, log_str};
-use near_sdk::{near_bindgen, AccountId, BorshStorageKey};
 use near_sdk::collections::UnorderedMap;
+use near_sdk::env::{self, log_str};
+use near_sdk::{near, AccountId, BorshStorageKey};
 
-#[derive(BorshSerialize, BorshStorageKey)]
-#[borsh(crate = "near_sdk::borsh")]
+#[near(serializers = [borsh])]
+#[derive(BorshStorageKey)]
 enum StorageKey {
     Points,
 }
 
 pub(crate) fn simulate_coin_flip() -> String {
-  // Here we get a first byte of a random seed
-  let random_seed = *env::random_seed().get(0).unwrap() as i8;
+    // Here we get a first byte of a random seed
+    let random_seed = *env::random_seed().get(0).unwrap() as i8;
 
-  // If a first byte is EVEN we choose heads, otherwise tails
-  if let 0 = random_seed % 2 {
-      return "heads".to_string()
-  } else {
-      return "tails".to_string()
-  };
+    // If a first byte is EVEN we choose heads, otherwise tails
+    if let 0 = random_seed % 2 {
+        return "heads".to_string();
+    } else {
+        return "tails".to_string();
+    };
 }
 
 // Define the contract structure
-#[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
-#[borsh(crate = "near_sdk::borsh")]
+#[near(contract_state)]
 pub struct Contract {
     points: UnorderedMap<AccountId, u8>,
 }
@@ -34,42 +31,42 @@ pub struct Contract {
 impl Default for Contract {
     fn default() -> Self {
         Self {
-            points: UnorderedMap::new(StorageKey::Points)
+            points: UnorderedMap::new(StorageKey::Points),
         }
     }
 }
 
 // Implement the contract structure
-#[near_bindgen]
+#[near]
 impl Contract {
     /*
       Flip a coin. Pass in the side (heads or tails) and a random number will be chosen
       indicating whether the flip was heads or tails. If you got it right, you get a point.
     */
     pub fn flip_coin(&mut self, player_guess: String) -> String {
-      // Check who called the method
-      let player: AccountId = env::predecessor_account_id();
-      log_str(&format!("{player} chose {player_guess}"));
+        // Check who called the method
+        let player: AccountId = env::predecessor_account_id();
+        log_str(&format!("{player} chose {player_guess}"));
 
-      // Simulate a Coin Flip
-      let outcome = simulate_coin_flip();
-      
-      // Get the current player points
-      let mut player_points = self.points.get(&player).unwrap_or(0);
+        // Simulate a Coin Flip
+        let outcome = simulate_coin_flip();
 
-      // Check if their guess was right and modify the points accordingly
-      if outcome.eq(&player_guess) {
-        player_points = player_points + 1;
-      } else {
-        player_points = player_points.saturating_sub(1);
-      };
+        // Get the current player points
+        let mut player_points = self.points.get(&player).unwrap_or(0);
 
-      log_str(&format!("player_points: {player_points}"));
+        // Check if their guess was right and modify the points accordingly
+        if outcome.eq(&player_guess) {
+            player_points = player_points + 1;
+        } else {
+            player_points = player_points.saturating_sub(1);
+        };
 
-      // Store the new points
-      self.points.insert(&player, &player_points);
+        log_str(&format!("player_points: {player_points}"));
 
-      return outcome;
+        // Store the new points
+        self.points.insert(&player, &player_points);
+
+        return outcome;
     }
 
     // View how many points a specific player has
