6b410bf52873d44deb6be66cbdcdfa66efb33ab0
Matias Benary <matiasbenary@gmail.com>
2024-12-20 21:07:08 -0300

    feat(ui): update packages and improve UX/UI

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -8,13 +8,13 @@ import { Wallet } from '@/wallets/near';
 import { NetworkId, CoinFlipContract } from '@/config';
 
 // Wallet instance
-// const wallet = new Wallet({ networkId: NetworkId });
+const wallet = new Wallet({ networkId: NetworkId });
 
 // Optional: Create an access key so the user does not need to sign transactions. Read more about access keys here: https://docs.near.org/concepts/protocol/access-keys
-const wallet = new Wallet({
-  createAccessKeyFor: CoinFlipContract,
-  networkId: NetworkId,
-});
+// const wallet = new Wallet({
+//   createAccessKeyFor: CoinFlipContract,
+//   networkId: NetworkId,
+// });
 
 
 export default function MyApp({ Component, pageProps }) {
