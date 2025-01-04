b7ced3d27a6bfbaa3b6cfb8c6e8505d67b7e8ed8
Guillermo Alejandro Gallardo Diez <gagdiez@iR2.home>
2025-01-04 18:46:07 +0100

    minor fixes

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -7,14 +7,11 @@ import { Navigation } from '@/components/Navigation';
 import { Wallet } from '@/wallets/near';
 import { NetworkId, CoinFlipContract } from '@/config';
 
-// Wallet instance
-const wallet = new Wallet({ networkId: NetworkId });
-
-// Optional: Create an access key so the user does not need to sign transactions. Read more about access keys here: https://docs.near.org/concepts/protocol/access-keys
-// const wallet = new Wallet({
-//   createAccessKeyFor: CoinFlipContract,
-//   networkId: NetworkId,
-// });
+// Create an access key so the user does not need to sign transactions. Read more about access keys here: https://docs.near.org/concepts/protocol/access-keys
+const wallet = new Wallet({
+  createAccessKeyFor: CoinFlipContract,
+  networkId: NetworkId,
+});
 
 
 export default function MyApp({ Component, pageProps }) {
