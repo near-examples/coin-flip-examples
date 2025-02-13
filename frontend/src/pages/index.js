36598cdddac0b27c199984485ec952ea418d65d8
Amira Nasri <nasriamira88@gmail.com>
2025-02-13 14:45:26 +0100

    Update index.js

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState } from "react";
 
 import Coin from "@/components/Coin";
 import { useWalletSelector } from '@near-wallet-selector/react-hook';
